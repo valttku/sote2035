c01384373034eb29476449db41f157f33cfa7e04
valttku <valtteri.kuitula@metropolia.fi>
2026-01-12 12:28:58 +0200

    test commit

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,7 +3,7 @@ import AppLayout from "../components/AppLayout";
 export default function Home() {
   return (
     <AppLayout>
-      <h1>Hello</h1>
+      <h1>Hello :)</h1>
     </AppLayout>
   );
 }
