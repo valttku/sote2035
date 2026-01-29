7fa80dd4bafb9e3b980f8a49cef27fa7b1c90913
Aliisa Rokala <aliisarokala@protonmail.com>
2026-01-29 22:25:00 +0200

    update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -24,8 +24,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://sote2035-server.onrender.com",
-  "http://sote2035-client.onrender.com",
+  "https://sote2035-client.onrender.com",
 ];
 
 app.use(
