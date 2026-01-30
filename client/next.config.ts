cbb57a0cceea022af70db1bef55d73e65e3648d3
Aliisa Rokala <aliisarokala@protonmail.com>
2026-01-30 12:31:06 +0200

    update next.config.ts

--- client/next.config.ts
+++ client/next.config.ts
@@ -5,18 +5,12 @@ const nextConfig = {
   },
 
   async rewrites() {
-    if (process.env.NODE_ENV === "production") {
-      // Forward /api/... requests to your backend in production
-      rewrites: async () => [
-        {
-          source: "/api/v1/:path*",
-          destination: "https://sote2035-server.onrender.com/api/v1/:path*",
-        },
-      ];
-    }
-
-    // Locally, no rewrites needed
-    return [];
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:4000/api/:path*",
+      },
+    ];
   },
 };
 
