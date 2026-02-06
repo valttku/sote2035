c88a859dd22539ff9e7c82763972baf5a7f1574d
Aliisa Rokala <aliisarokala@protonmail.com>
2026-02-06 13:17:13 +0200

    fixed responsive sidebar issues, made calendar dot brighter, fixed issue with desktop mode padding

--- client/src/components/AppLayout.tsx
+++ client/src/components/AppLayout.tsx
@@ -4,18 +4,11 @@ import Sidebar from "./Sidebar/Sidebar";
 export default function AppLayout({ children }: { children: ReactNode }) {
   return (
     <div className="h-screen">
-      <Sidebar  />
+      <Sidebar />
 
-       {/* Main Content */}
-      <main className="flex-1 
-          min-h-screen
-          
-          p-4 sm:p-6 lg:p-8   // Responsive padding (mobile → tablet → desktop)
-
-          md:ml-64           // Leaves space for sidebar on desktop
-     "
-     >
-      {children}
+      {/* Main Content */}
+      <main className="flex-1 min-h-screen p-4 sm:p-6 lg:p-0 md:ml-64">
+        {children}
       </main>
     </div>
   );
